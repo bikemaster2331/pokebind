6501bdb31f08511a2e9a3f8c3647f1e15f7db9a5
bikemaster2331 <tanlanuzga@gmail.com>
2026-03-29 17:39:27 +0800

    Fix import path in admin cards API

--- app/api/admin/cards/route.js
+++ app/api/admin/cards/route.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-import { createSupabaseServer } from '../../server'
+import { createSupabaseServer } from '../../../server'
 
 const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
